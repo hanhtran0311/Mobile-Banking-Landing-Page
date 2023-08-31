a0d425eb3b36a1fefed02a312582e474a666e871
Tran Kim Hanh <hanhtran@MacBook-Air-cua-Tran.local>
2023-08-31 14:51:35 +0900

    Feature

--- main.js
+++
@@ -1,9 +0,0 @@
-// Làm bài
-function getCanVoteMessage(age) {
-  var canVote = age >= 18 ? "Bạn có thể bỏ phiếu" : "Bạn chưa được bỏ phiếu";
-  return canVote;
-}
-
-// Kỳ vọng
-console.log(getCanVoteMessage(18)); // 'Bạn có thể bỏ phiếu'
-console.log(getCanVoteMessage(15)); // 'Bạn chưa được bỏ phiếu'
